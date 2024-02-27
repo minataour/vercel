21cc2261804466407b48cbb2406c432c5144ceec
minataour <1njust1c104@gmail.com>
2024-02-28 01:48:41 +0530

    llll

--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,9 @@
 const bodyParser = require('body-parser');
-const books = require("express").Router();
+const items = require("express").Router();
 const { getStoredItems, storeItems } = require('../data/items');
 
-books.use(bodyParser.json());
-books.use((req, res, next) => {
+items.use(bodyParser.json());
+items.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -11,19 +11,19 @@ books.use((req, res, next) => {
 });
 
 
-books.get('/items', async (req, res) => {
+items.get('/items', async (req, res) => {
   const storedItems = await getStoredItems();
   await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
   res.json({ items: storedItems });
 });
 
-books.get('/items/:id', async (req, res) => {
+items.get('/items/:id', async (req, res) => {
   const storedItems = await getStoredItems();
   const item = storedItems.find((item) => item.id === req.params.id);
   res.json({ item });
 });
 
-books.post('/items', async (req, res) => {
+items.post('/items', async (req, res) => {
   const existingItems = await getStoredItems();
   const itemData = req.body;
   const newItem = {
@@ -36,4 +36,4 @@ books.post('/items', async (req, res) => {
 });
 
 
-module.exports = books;
+module.exports = items;
