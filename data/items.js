9d3469ec5b307aebb3d44bd2002d8eb2e7b2d31c
minataour <1njust1c104@gmail.com>
2024-03-01 12:35:38 +0530

    jjj

--- data/items.js
+++ data/items.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs/promises');
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  const rawFileContent = await fs.readFile('tmp/items.json', { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
